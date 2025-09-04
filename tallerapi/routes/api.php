5067728834f96d1962a7f93c2b629069a7a0349b
Nicolle Melendez <nicollev2020@gmail.com>
2025-09-04 08:50:17 -0500

    Adiccion de la ruta unit
    se adicciona la ruta unit

--- tallerapi/routes/api.php
+++ tallerapi/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UnitController;
 use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('unit', UnitController::class);
 
 Route::apiResource('article', ArticleController::class);
