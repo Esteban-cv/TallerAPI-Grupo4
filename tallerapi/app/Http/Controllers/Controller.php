320f261ffde2173f1817a0b796721e9018b56ce2
Esteban C.v <stphgamer@gmail.com>
2025-09-04 08:59:22 -0500

    Edicion del metodo applyvalidatorion

--- tallerapi/app/Http/Controllers/Controller.php
+++ tallerapi/app/Http/Controllers/Controller.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class Controller extends BaseController
