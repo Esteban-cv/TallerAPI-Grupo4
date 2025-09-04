2d1c6827a1ce1ea9d390f64b8a4ce46b2bb34d10
Esteban C.v <stphgamer@gmail.com>
2025-09-04 08:47:43 -0500

    Corrección del handler y finalización del ArticleController para el funcionamiento de la api

--- tallerapi/app/Exceptions/Handler.php
+++ tallerapi/app/Exceptions/Handler.php
@@ -2,9 +2,13 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -20,6 +24,19 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    private $url = [
+        'article',
+        'category',
+        'entry',
+        'issue',
+        'person',
+        'presentation',
+        'role',
+        'supplier',
+        'unit',
+        'user'
+    ];
+
     /**
      * Register the exception handling callbacks for the application.
      */
@@ -28,18 +45,39 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            //AÑADIR EL PREFIJO A LA LISTA DE URLS
+            $urlFinal = preg_filter('/^/', 'api/', $this->url);
+            //AÑADIR EL SUFIJO / A LA LISTA DE URLS
+            $urlFinal = preg_filter('/$/', '/*', $urlFinal);
+
+            if ($request->is($urlFinal)) {
+                return response()->json([
+                    'message' => 'Registro no encontrado'
+                ], Response::HTTP_NOT_FOUND);
+            }
+        });
+
+        $this->renderable(function (MethodNotAllowedHttpException $e, $request) {
+            return response()->json([
+                'message' => 'Metodo no encontrado o soportado'
+            ], Response::HTTP_METHOD_NOT_ALLOWED);
+        });
     }
 
     public function render($request, Throwable $e) {
-        if ($e instanceof NotFoundHttpException) {
-            return response()->view('errors.404', [], 404);
-        }        
-
-        if($e instanceof UnauthorizedHttpException)
-        {
-            return response()->view('errors.403', [], 403);
+        if ($e instanceof AuthorizationException) {
+            return response()->json([
+                'message' => 'Acceso denegado'
+            ],Response::HTTP_FORBIDDEN);
+        }
+        if ($e instanceof RouteNotFoundException) {
+            return response()->json([
+                'message' => 'Debe iniciar sesion'
+            ],Response::HTTP_UNAUTHORIZED);
         }
 
-        return parent::render($request, $e);
+        return parent::render($request,$e);
     }
 }
--- tallerapi/app/Http/Controllers/ArticleController.php
+++ tallerapi/app/Http/Controllers/ArticleController.php
@@ -2,16 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ArticleController extends Controller
 {
+
+    private $rules = [
+        'name' => 'required|string|min:3|max:100',
+        'quantity' => 'required|numeric|min:1|max:9999999999',
+        'photo' => 'max:255',
+        'technical_sheet' => 'mimes:pdf|max:5120',
+        'presentation_id' => 'max:9999999999999999999',
+        'category_id' => 'max:9999999999999999999',
+        'supplier_id' => 'max:9999999999999999999',
+        'unit_id' => 'max:9999999999999999999'
+    ];
+
+    private $traductionAttributes = [
+        'name' => 'nombre',
+        'quantity' => 'cantidad',
+        'min_quantity' => 'cantidad minima',
+        'photo' => 'foto',
+        'technical_sheet' => 'ficha técnica',
+        'presentation_id' => 'presentación',
+        'category_id' => 'categoría',
+        'supplier_id' => 'proveedor',
+        'unit_id' => 'unidad'
+    ];
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $articles = Article::all();
+        $articles->load(['presentation','category','supplier','unit']);
+        return response()->json($articles,Response::HTTP_OK);
     }
 
     /**
@@ -19,30 +47,54 @@ class ArticleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $this->applyValidator($request, $this->rules, $this->traductionAttributes);
+        if (!empty($data)) {
+            return $data;
+        }
+        $article = Article::create($request->all());
+        $response = [
+            'message' => 'Artículo creado exitosamente',
+            'article' => $article
+        ];
+        return response()->json($response, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Article $article)
     {
-        //
+        $article->load(['presentation','category','supplier','unit']);
+        return response()->json($article,Response::HTTP_OK);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Article $article)
     {
-        //
+        $data = $this->applyValidator($request, $this->rules, $this->traductionAttributes);
+        if (!empty($data)) {
+            return $data;
+        }
+        $article->update($request->all());
+        $response = [
+            'message' => 'Artículo actualizado exitosamente',
+            'art$article' => $article
+        ];
+        return response()->json($response, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Article $article)
     {
-        //
+        $article->delete();
+        $response = [
+            'message' => 'Orden eliminada exitosamente',
+            'article$article' => $article
+        ];
+        return response()->json($response, Response::HTTP_OK);
     }
 }
