0f95cc3c2f883bde1ca8027e5d037d5e3f2e3c0b
Juan_Diego <juan3312orrego@gmail.com>
2025-09-04 08:58:41 -0500

    Rutas iniciales de la API

--- tallerapi/routes/api.php
+++ tallerapi/routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\EntryController;
+use App\Http\Controllers\IssueController;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\PresentationController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +29,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('article', ArticleController::class);
+Route::apiResource('category', CategoryController::class);
+Route::apiResource('entry', EntryController::class);
+Route::apiResource('issue', IssueController::class);
+Route::apiResource('person', PersonController::class);
+Route::apiResource('presentation', PresentationController::class);
+Route::apiResource('role', RoleController::class);
+Route::apiResource('supplier', SupplierController::class);
+Route::apiResource('unit', UnitController::class);
+Route::apiResource('user', UserController::class);
